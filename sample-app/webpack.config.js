d9c17678106fc4f279c48e3940c2cba7083eacb3
Gergely Szabó <gergely0116@freemail.hu>
2018-09-19 20:17:15 +0200

    configuring webpack

---
+++ sample-app/webpack.config.js
@@ -0,0 +1,20 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/client/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: './dist/bundle.js'
+  },
+  module: {
+    rules: [
+      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
+    ]
+  },
+  devServer: {
+    port: 3000,
+    open: true
+  }
+}
