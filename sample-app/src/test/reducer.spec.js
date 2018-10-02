1ca20b70eaedf109564b4789e1ac5efc65b2fd94
Gergely Szabó <gergely0116@freemail.hu>
2018-10-02 21:16:51 +0200

    adding more tests

--- sample-app/src/test/reducer.spec.js
+++ sample-app/src/test/reducer.spec.js
@@ -27,4 +27,29 @@ describe('Reducer tests', () => {
     expect(newState.xWinCount).toBe(1);
     expect(newState.oWinCount).toBe(0);
   });
+
+  it('Should change the currnt player after CHANGE_TURNS action', () => {
+    const testAction = {
+      type: 'CHANGE_TURNS', 
+      payload: {
+        currentPlayer: 'x'
+      }
+    };
+    let newState = reducer(testState, testAction);
+    expect(newState.currentPlayer).toBe('o');
+  });
+
+  it('Should set the currentWinner on GAME_OVER action', () => {
+    const testAction = {
+      type: 'GAME_OVER', 
+      payload: {
+        winner: 'x'
+      }
+    };
+    let newState = reducer(testState, testAction);
+    expect(newState.currentWinner).toBe('x');
+    expect(newState.xWinCount).toBe(2);
+    expect(newState.oWinCount).toBe(0);
+  });
+
 });
